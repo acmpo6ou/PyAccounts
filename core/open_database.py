82bc2080141e6dd316e50214ad9dfdb3d73fd6e8
acmpo6ou <acmpo6ou@gmail.com>
2021-10-15 17:59:03 +0300

    Add stub for on_open_database

---
+++ core/open_database.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+#  Copyright (c) 2022. Bohdan Kolvakh
+#  This file is part of PyAccounts.
+#
+#  PyAccounts is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  PyAccounts is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with PyAccounts.  If not, see <https://www.gnu.org/licenses/>.
+
+import gi
+
+gi.require_version("Gtk", "3.0")
+from core.gtk_utils import GladeTemplate
+
+
+class OpenDatabaseForm(GladeTemplate):
+    def __init__(self):
+        super().__init__("open_database")
+        self.vexpand = True
+
+    def on_open_database(self, _):
+        """
+        Tries to open database with password from password field, if there is an error decrypting
+        the database – displays `incorrect password` tip.
+        """
