4bafcce979b9d9fde1cf02eb7b0dbacd4f18ec66
acmpo6ou <acmpo6ou@gmail.com>
2021-10-17 13:27:30 +0300

    Write much better docs for on_generate
    Add stub with docs for genpass

--- core/generate_password.py
+++ core/generate_password.py
@@ -20,6 +20,9 @@ import gi
 
 gi.require_version("Gtk", "3.0")
 from core.gtk_utils import GladeTemplate
+from string import digits, ascii_lowercase, ascii_uppercase, punctuation
+
+ALL_CHARS = (digits, ascii_lowercase, ascii_uppercase, punctuation)
 
 
 class GenPassDialog(GladeTemplate):
@@ -29,15 +32,39 @@ class GenPassDialog(GladeTemplate):
 
     def __init__(self, pass1, pass2):
         super().__init__("generate_password")
+        self.checkboxes = (self.numbers, self.lower, self.upper, self.punctuation)
+
         self.pass1 = pass1
         self.pass2 = pass2
 
     def on_cancel(self, _):
         self.parent_widget.hide()
 
+    def genpass(self, length, chars):
+        """
+        Generates random password.
+
+        :param length: length of password to generate.
+        :param chars: characters from which to generate password.
+        :return: generated random password.
+        """
+        # Because password generates randomly it won't necessarily contain all characters that
+        # are specified in `chars`.
+        # So here we check that generated password contains at least one character from each string
+        # specified in `chars` and if it doesn't, we regenerate password
+
     def on_generate(self, _):
         """
         Generates password and fills pass1 and pass2 password fields with it.
         """
-        # TODO: regenerate password if it happens not to contain at least one of the specified
-        #  by user characters
+        """
+        chars = []
+        For each index, checkbox in enumerate checkboxes:
+            if it is checked:
+                get characters associated with that checkbox (using ALL_CHARS[index])
+                add them to chars
+
+        call genpass passing it chars and password length
+        fill password fields with generated password
+        hide dialog
+        """
