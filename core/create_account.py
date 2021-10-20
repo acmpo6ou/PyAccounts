4c179374d0d7bc10f8f3eec253d6be7e922e6c69
acmpo6ou <acmpo6ou@gmail.com>
2021-10-20 09:12:10 +0300

    Implement on_hover_date_icon for CreateAccountForm

---
+++ core/create_account.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+#  Copyright (c) 2022. Bohdan Kolvakh
+#  This file is part of PyAccounts.
+#
+#  PyAccounts is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  PyAccounts is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with PyAccounts.  If not, see <https://www.gnu.org/licenses/>.
+
+import gi
+
+gi.require_version("Gdk", "3.0")
+
+from gi.repository import Gdk
+from core.gtk_utils import GladeTemplate
+
+
+class CreateAccountForm(GladeTemplate):
+    def __init__(self):
+        super().__init__("create_edit_account")
+
+    @staticmethod
+    def on_hover_date_icon(icon):
+        """
+        Changes cursor style to `pointer` when hovering over date icon.
+        :param icon: the date icon.
+        """
+        icon.window.cursor = Gdk.Cursor(Gdk.CursorType.HAND1)
