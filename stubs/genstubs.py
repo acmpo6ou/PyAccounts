c86ed61278278a46ed664dcc78de167590e867f6
acmpo6ou <acmpo6ou@gmail.com>
2021-10-30 11:12:32 +0300

    Implement getting glade ui file name

--- stubs/genstubs.py
+++ stubs/genstubs.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python3
 
 import os
+import re
+import xml.etree.ElementTree as ET
 
-os.system("stubgen core -o stubs")
+# os.system("stubgen core -o stubs")
+
 STUB_DIR = "stubs/core/"
+SKIP = ("__init__.pyi", "gtk_utils.pyi", "database_utils.pyi", "widgets.pyi")
 
 for stub in os.listdir(STUB_DIR):
-    if stub == "__init__.pyi":
+    if stub in SKIP:
         continue
 
     _in = open(f"{STUB_DIR}/{stub}", "r").readlines()
-    out = open(f"{STUB_DIR}/{stub}", "w")
+    # out = open(f"{STUB_DIR}/{stub}", "w")
 
+    injected = False
+    # print(stub)
     for line in _in:
-        if "def " not in line and ": Any" in line:
-            continue
+        # out.write(line)
 
-        out.write(line)
+        if "class " in line and not injected:
+            injected = True
+            glade_file = f"ui/{stub[:-3]}glade"
+
+            regex = re.search('id="(*)"')
+
+            # root = ET.parse(glade_file).getroot()
+            # print(root)
+    break
