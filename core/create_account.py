f6a94dd315651a8ce108ea974f5f2bc66d5fb013
acmpo6ou <acmpo6ou@gmail.com>
2021-11-01 11:46:52 +0200

    Add note to get_attached_files about None paths

--- core/create_account.py
+++ core/create_account.py
@@ -143,6 +143,8 @@ class CreateAccountForm(CreateForm):
 
         # TODO: create empty attached_files dict
         # TODO: iterate through attached_paths:
+        #  if path is None – skip it, because EditAccountForm may add old attached files as items
+        #  with None path
         #  for each path try to read file content; save the content to attached_files dict
         #  on error display ErrorDialog "Failed to read file [file name]"
 
