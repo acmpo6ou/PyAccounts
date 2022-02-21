b613943c0541c588c409afae8e46a5f8dd8a50bb
acmpo6ou <acmpo6ou@gmail.com>
2022-02-21 09:51:00 +0200

    💥 Remove redundant account fixture
    Introduce a few globals instead

--- tests/test_database_utils.py
+++ tests/test_database_utils.py
@@ -13,26 +13,47 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with PyAccounts.  If not, see <https://www.gnu.org/licenses/>.
-import pytest
 
 from core.database_utils import Account, Database
 
 
-@pytest.fixture
-def account():
-    return Account(
-        accountname="gmail",
-        username="Gmail User",
-        email="example@gmail.com",
-        password="123",
-        birthdate="01.01.2000",
-        notes="My gmail account.",
-        copy_email=False,
-        attached_files={"file1": "file1 content", "file2": "file2 content"},
-    )
+account = Account(
+    accountname="gmail",
+    username="Gmail User",
+    email="example@gmail.com",
+    password="123",
+    birthdate="01.01.2000",
+    notes="My gmail account.",
+    copy_email=False,
+    attached_files={"file1": "file1 content", "file2": "file2 content"},
+)
+
+account2 = Account(
+    accountname="mega",
+    username="Mega User",
+    email="example@gmail.com",
+    password="312",
+    birthdate="05.01.2000",
+    notes="My mega account.",
+)
+
+accounts = {
+    account.accountname: account,
+    account2.accountname: account2,
+}
+
+ACCOUNTS_JSON = (
+    '{"gmail": {"account": "gmail", "name": "Gmail User", "email": '
+    '"example@gmail.com", "password": "123", "date": "01.01.2000", "comment": "My '
+    'gmail account.", "copy_email": false, "attach_files": {"file1": "file1 '
+    'content", "file2": "file2 content"}}, "mega": {"account": "mega", "name": '
+    '"Mega User", "email": "example@gmail.com", "password": "312", "date": '
+    '"05.01.2000", "comment": "My mega account.", "copy_email": true, '
+    '"attach_files": {}}}'
+)
 
 
-def test_account_to_dict(account):
+def test_account_to_dict():
     account_dict = account.to_dict()
 
     expected_dict = {
@@ -56,28 +77,7 @@ def test_database_opened():
     assert not without_password.opened
 
 
-def test_dumps(account):
-    account2 = Account(
-        accountname="mega",
-        username="Mega User",
-        email="example@gmail.com",
-        password="312",
-        birthdate="05.01.2000",
-        notes="My mega account.",
-    )
-    database = Database(
-        "main", "123", {account.accountname: account, account2.accountname: account2}
-    )
-
+def test_dumps():
+    database = Database("main", "123", accounts)
     json = database.dumps()
-
-    expected_json = (
-        '{"gmail": {"account": "gmail", "name": "Gmail User", "email": '
-        '"example@gmail.com", "password": "123", "date": "01.01.2000", "comment": "My '
-        'gmail account.", "copy_email": false, "attach_files": {"file1": "file1 '
-        'content", "file2": "file2 content"}}, "mega": {"account": "mega", "name": '
-        '"Mega User", "email": "example@gmail.com", "password": "312", "date": '
-        '"05.01.2000", "comment": "My mega account.", "copy_email": true, '
-        '"attach_files": {}}}'
-    )
-    assert json == expected_json
+    assert json == ACCOUNTS_JSON
