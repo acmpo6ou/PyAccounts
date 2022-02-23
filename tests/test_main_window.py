3f5d2f3f48725cfa2c701e62c15abea272669f5f
acmpo6ou <acmpo6ou@gmail.com>
2022-02-23 12:07:04 +0200

    ✔ Write test for get_databases

---
+++ tests/test_main_window.py
@@ -0,0 +1,31 @@
+#  Copyright (c) 2021-2022. Bohdan Kolvakh
+#  This file is part of PyAccounts.
+#
+#  PyAccounts is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  PyAccounts is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with PyAccounts.  If not, see <https://www.gnu.org/licenses/>.
+import shutil
+
+from core.database_utils import Database
+from core.main_window import MainWindow
+
+
+def test_get_databases(src_dir):
+    shutil.copy("tests/data/main.dba", src_dir)
+    shutil.copy("tests/data/main.dba", src_dir / "crypt.dba")
+    shutil.copy("tests/data/main.dba", src_dir / "data.dba")
+
+    main_window = MainWindow()
+    dbs = main_window.get_databases()
+
+    expected_dbs = [Database("main"), Database("crypt"), Database("data")]
+    assert dbs == expected_dbs
