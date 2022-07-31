86335b39fd27d48dc31a878401c7449d968227c6
acmpo6ou <acmpo6ou@gmail.com>
2022-07-31 19:45:31 +0200

    🐛 Fix pyaccounts.sh

--- deb/pyaccounts.sh
+++ deb/pyaccounts.sh
@@ -2,4 +2,4 @@
 
 cd /usr/share/pyaccounts/PyAccounts
 source ../bin/activate
-python3.9 PyAccounts.py "$@" 2>/tmp/`date +'%d-%m-%Y_%H:%M:%S'`-PyAccounts.log
+../bin/python3.9 PyAccounts.py "$@" 2>/tmp/`date +'%d-%m-%Y_%H:%M:%S'`-PyAccounts.log
