323697cac2a7c15d8414a412d797905b4a845c4e
acmpo6ou <acmpo6ou@gmail.com>
2023-09-30 21:21:16 +0200

    🐞 Fix file dialog crash on MacOS

--- PyAccounts.sh
+++ PyAccounts.sh
@@ -2,4 +2,5 @@
 
 cd ~/Documents/PyAccounts || exit
 source venv/bin/activate
+export GSETTINGS_SCHEMA_DIR=/opt/homebrew/share/glib-2.0/schemas/
 GTK_THEME=Adwaita:dark XDG_DATA_DIRS=./share ./PyAccounts.py
